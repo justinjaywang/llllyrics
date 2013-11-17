d6c20eb418dfe19b38250271879945bfb09c6a8b
justinjaywang <justinjaywang@gmail.com>
2013-11-16 18:52:06 -0800

    Added autocomplete feature on edit and add pages

--- js/song.js
+++ js/song.js
@@ -70,11 +70,17 @@ app.directive('focus', function($timeout) {
   }
 });
 
-app.directive('searchInputHandler', function($timeout) {
+app.directive('searchInputHandler', function() {
   return function(scope, element) {
     element.bind('focus', function() {
       scope.updateSearchTerm();
     });
+
+    // element.bind('keydown', function(e) {
+    //   if (e.which === 40) { // down
+    //       e.preventDefault();
+    //     }
+    //   });
   }
 });
 
@@ -93,21 +99,19 @@ app.directive('pageHandler', function($location) {
         } else {
           searchInput.focus();
         }
-      }
+      } 
     });
 
     element.bind('keypress', function(e) {
       var l = $location.path();
 
       if (l == '/add') return;
-
       if (l.indexOf('/view/') == 0) { // view page
         if (e.which === 101) { // e
           scope.$apply($location.path('/edit/' + l.substring('6')));
           return;
         }
       }
-
       if (document.getElementById('search-input') != document.activeElement) {
         if (e.which === 43) { // +
           e.preventDefault();
@@ -117,30 +121,193 @@ app.directive('pageHandler', function($location) {
           });
         }
       }
+    }); // end keydown
+
+  }
+});
+
+app.directive('infoInputHandler', function($location, $timeout, $compile) { // to do: remove inputs later as necessary
+  return function(scope, element, attrs) {
+    var type = attrs.infoType;
+    var isArtist = (type == 'artist');
+    var $ul = (isArtist) ? angular.element(document.getElementById('artistAutocomplete')) : angular.element(document.getElementById('albumAutocomplete'));
+    var selectedIndex = 0;
+
+    var showAutocomplete = function(e) {
+
+      // var $ul = identifyUl();
+      $ul = toggleAutocomplete($ul, false);
+
+      // if (typeof scope.song === 'undefined') return;
+      // if (typeof scope.song.artist === 'undefined') return;
+      // // to do: add album undefined case
+      if (typeof scope.song === 'undefined') { // to do: remove later if unnecessary
+        console.log('scope.song undefined in showAutocomplete');
+        return;
+      }
+
+      if (typeof scope.song.artist === 'undefined') return;
+      if (!isArtist && (scope.song.album === '' || typeof scope.song.album === 'undefined')) return; // album, null case
+      if (e.which === 27) return; // esc
+
+      // var charArray = [16,18,32,38,40,48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,96,97,98,99,100,101,102,103,104,105,186,187,188,189,190,191,191,219,220,221,222];
+      // var isCharacter = charArray.some(function(element) { return (e.which === element) });
+      // // if (!isCharacter) return;
+
+      var updateMatches = function(matches, currKey) {
+        var matched = false;
+        matched = matches.some(function(element) {
+          if (currKey==element.key) {
+            element.count++; // increment count
+            return true;
+          }
+          return false;
+        });
+        if (!matched) matches.push({'key': currKey, 'count': 1}); // add new object
+        return matches;
+      };
+
+      var matches = []; // initialize
+      var limitTo = 5;
+      var artistQuery = scope.song.artist;
+
+      // populate matches
+      if (isArtist) {
+        scope.songs.forEach(function(element) {
+          var artist = element.artist;
+          var isMatch = (artist.toLowerCase().indexOf(artistQuery.toLowerCase())!=-1);
+          if (isMatch) matches = updateMatches(matches, artist);
+        });
+      } else if (typeof artistQuery !== 'undefined') { // album, if artistQuery is not empty
+        var albumQuery = scope.song.album;
+        scope.songs.forEach(function(element) { // album
+          if (typeof element.album === 'undefined') return; // skip songs with undefined albums
+
+          var artist = element.artist;
+          var album = element.album;
+          var isArtistMatch = (artist == artistQuery);
+          var isAlbumMatch = (album.toLowerCase().indexOf(albumQuery.toLowerCase())!=-1);
+          var isMatch = (isArtistMatch && isAlbumMatch);
+
+          if (isMatch) matches = updateMatches(matches, album);
+        });
+      }
+
+      // sort matches
+      matches.sort(function(a, b) {
+        return b.count - a.count;
+      });
+
+      // truncate matches
+      matches.length = limitTo;
+
+      // append matches
+      $ul = toggleAutocomplete($ul, true); // show autocomplete
+      matches.forEach(function(element, index) {
+        var $li = angular.element('<li info-type="' + type + '" change-input>' + element.key + '</li>');
+        if (index==selectedIndex) $li.addClass('selected');
+        $ul.append($compile($li)(scope));
+      });
+
+      // console.log('show ul')
+      // selectedIndex = 0;
+    };
+
+    var toggleAutocomplete = function($ul, show) {
+      $ul.html('');
+      if ($ul.hasClass('show-' + !show)) {
+        $ul.removeClass('show-' + !show);
+        $ul.addClass('show-' + show);
+      }
+      return $ul;
+    };
+
+    var preventDefaults = function(e) {
+      var k = e.which;
+      var isAutocompleteShown = $ul.hasClass('show-true');
+
+      if (!isAutocompleteShown) return;
+
+      if (k === 40) { // down
+        e.preventDefault();
+        if (selectedIndex >= ($ul.children().length-1)) return;
+        // updateItemStyle(selectedIndex, ++selectedIndex);
+        selectedIndex++;
+        // console.log(selectedIndex);
+        // console.log('down')
+      } else if (k === 38) { // up
+        e.preventDefault();
+        // increment if possible
+        if (selectedIndex == 0) return;
+        // updateItemStyle(selectedIndex, --selectedIndex);
+        selectedIndex--;
+        // console.log(selectedIndex);
+        // console.log('up')
+      } else if ((k === 9) || (k === 13)) { // tab or enter
+        e.preventDefault();
+        selectIndex();
+      }
+    };
+
+    var selectIndex = function() {
+      var nextInput = (isArtist) ? document.getElementById('album-input') : document.getElementById('song-input');
+
+      scope.$apply(function() {
+        $timeout(function() { 
+          nextInput.focus();
+        }, 150);
+        if (isArtist) {
+          scope.song.artist = $ul.children()[selectedIndex].innerText;
+        } else {
+          scope.song.album = $ul.children()[selectedIndex].innerText;
+        }
+      });
+    };
+
+    var updateItemStyle = function(oldIndex, newIndex) {
+      // console.log('old: ', oldIndex);
+      // console.log('new: ', newIndex);
+      $ul.children()[oldIndex].className = '';
+      $ul.children()[newIndex].className = 'selected';
+    }
+
+    element.bind('keydown', preventDefaults);
+    element.bind('keyup', showAutocomplete);
+    element.bind('blur', function() {
+      toggleAutocomplete($ul, false);
+    });
+    element.bind('focus', function() {
+      selectedIndex = 0; // reset index
     });
 
   }
 });
 
-app.directive('infoInputHandler', function($location, $timeout) { // to do: remove inputs later as necessary
+app.directive('changeInput', function($timeout) {
   return function(scope, element, attrs) {
 
-    element.bind('keyup', function(e) {
-      // console.log(attrs.ngModel);
-      // console.log('keypress');
-      if (typeof scope.song === 'undefined') return;
-      var artist = scope.song.artist;
+    var selectItem = function() {
+      var isArtist = (attrs.infoType == 'artist');
+      var nextInput = (isArtist) ? document.getElementById('album-input') : document.getElementById('song-input');
 
-      console.log(scope.song.artist);
-    });
+      scope.$apply(function() {
+        $timeout(function() { 
+          nextInput.focus();
+        }, 150);
+        if (isArtist) {
+          scope.song.artist = element[0].innerText;
+        } else {
+          scope.song.album = element[0].innerText;
+        }
+      });
 
-    // element.bind('keydown', function(e) {
-      // console.log('keydown');
-    // });
+    };
+
+    element.bind('mousedown', selectItem);
   }
 });
 
-app.directive('removeLater', function() {
+app.directive('removeLater', function() { // to do: remove
   return function(scope, element, attrs) {
     element.bind('keydown', function(e) {
 
@@ -224,23 +391,13 @@ function TitleCtrl($scope, $rootScope, Page) {
     $rootScope.searchTerm = '';
   }
 
-  // $rootScope.artistSearchFunction = function(song) {
-  //   console.log($scope.song)
-  //   // console.log(song)
-  //   // console.log('gets to artistSearchFunction')
-  //   // if (typeof song === 'undefined') {
-  //     // console.log('song undefined');
-  //     // return false;
-  //   // }
-  //   // console.log($scope.song) // never gets here
-  //   // if ($scope.song.artist == 'test') {
-  //   //   console.log('gets to true')
-  //   //   return true;
-  //   // } else {
-  //   //   return false;
-  //   // }
-  //   // return false;
-  // }
+  // $rootScope.changeInput = function(newInput) {
+  //   // console.log('testFunction!')
+  //   // console.log(newInput);
+  //   console.log($scope.song.artist);
+  //   // $scope.song.artist = newInput;
+  //   console.log(newInput);
+  // };
 }
 
 function SearchCtrl($scope, $rootScope, $timeout, Page, Song) {
@@ -292,6 +449,7 @@ function SearchCtrl($scope, $rootScope, $timeout, Page, Song) {
 
     if (searchTermSimple.length < 2) return false; // simplified search term is short, don't return results
     else { // longer than 1, start searching
+
       var searchTermArray = splitSearchTerm(searchTermSimple.toLowerCase());
       var l = searchTermArray.length;
       var artist = simplifyReference(song.artist).toLowerCase();
@@ -299,9 +457,10 @@ function SearchCtrl($scope, $rootScope, $timeout, Page, Song) {
       var title = simplifyReference(song.song).toLowerCase();
       var lyrics = simplifyReference(song.lyrics).toLowerCase();
 
-      for (var i=0; i<l; i++) { // for each word, check if match
-        
-        var sOrig = searchTermArray[i];
+      // to do: convert to forEach or every
+      var matchesAll = searchTermArray.every(function(sOrig, i) { // check if match on all words
+
+        if (i == (l-1)) showInput(); // show search input on last iteration
 
         if (sOrig.indexOf('artist:') == 0) { // artist search
           var s = simplifyReference(sOrig.substring(7).replace(/_/g, ' '));
@@ -321,15 +480,11 @@ function SearchCtrl($scope, $rootScope, $timeout, Page, Song) {
         }
 
         if (!isMatch) return false; // if ever not a match, return false
+        return true;
 
-        if (i == (l-1)) { // last iteration, has not encountered return false so it is a match
-          showInput();
-          return true;
-        } else {
-          continue;
-        }
-        
-      }
+      });
+
+      return matchesAll;
     }
   }; // end searchFunction
 
@@ -384,81 +539,13 @@ function AddCtrl($scope, $location, $timeout, Page, Song) {
 
   $scope.shouldFocus = true;
 
-  $scope.artistSearchFunction = function(song) {
+  // $scope.changeInput = function(newInput) {
+  //   // console.log('testFunction!')
+  //   // console.log(newInput);
+  //   // console.log($scope.song.artist);
+  //   $scope.song.artist = newInput;
+  // };
 
-    var s = $scope.song;
-    if (typeof s === 'undefined') return false;
-    if (typeof s.artist === 'undefined') return false;
-
-    // if (searchTermSimple.length < 2) return false; // simplified search term is short, don't return results
-    // else { // longer than 1, start searching
-      // var sArray = splitSearchTerm((s.artist).toLowerCase());
-      // var l = sArray.length;
-    var a = s.artist.toLowerCase();
-    var artist = (song.artist).toLowerCase();
-      // var album = (song.album) ? (song.album).toLowerCase() : null;
-
-      console.log('s: ', s.artist);
-      // console.log('l: ', l);
-      console.log('artist: ', artist);
-
-    if (artist.indexOf(a) != -1) { // match artist
-      return true;
-    } else {
-      return false;
-    }
-      // console.log('album: ', album);
-      // var title = simplifyReference(song.song).toLowerCase();
-      // var lyrics = simplifyReference(song.lyrics).toLowerCase();
-
-      // for (var i=0; i<l; i++) { // for each word, check if match
-        
-      //   var sOrig = searchTermArray[i];
-
-      //   if (sOrig.indexOf('artist:') == 0) { // artist search
-      //     var s = simplifyReference(sOrig.substring(7).replace(/_/g, ' '));
-      //     var isMatch = (artist.indexOf(s)!=-1);
-      //   } else if (sOrig.indexOf('album:') == 0) { // album search
-      //     var s = simplifyReference(sOrig.substring(6).replace(/_/g, ' '));
-      //     var isMatch = (album) ? (album.indexOf(s)!=-1) : false;
-      //   } else if (sOrig.indexOf('songtitle:') == 0) { // song title search
-      //     var s = simplifyReference(sOrig.substring(10).replace(/_/g, ' '));
-      //     var isMatch = (title.indexOf(s)!=-1);
-      //   } else if (sOrig.indexOf('lyrics:') == 0) { // lyrics search
-      //     var s = simplifyReference(sOrig.substring(7).replace(/_/g, ' '));
-      //     var isMatch = (lyrics.indexOf(s)!=-1);
-      //   } else { // default search
-      //     var s = simplifyReference(sOrig); // remove : _ "
-      //     var isMatch = (album) ? (artist.indexOf(s)!=-1 || album.indexOf(s)!=-1 || title.indexOf(s)!=-1 || lyrics.indexOf(s)!=-1) : (artist.indexOf(s)!=-1 || title.indexOf(s)!=-1 || lyrics.indexOf(s)!=-1);
-      //   }
-
-      //   if (!isMatch) return false; // if ever not a match, return false
-
-      //   if (i == (l-1)) { // last iteration, has not encountered return false so it is a match
-      //     showInput();
-      //     return true;
-      //   } else {
-      //     continue;
-      //   }
-        
-      // }
-
-    // }
-    // console.log(song)
-    // console.log('gets to artistSearchFunction')
-    // if (typeof song === 'undefined') {
-      // console.log('song undefined');
-      // return false;
-    // }
-    // console.log($scope.song) // never gets here
-    // if ($scope.song.artist == 'test') {
-    //   console.log('gets to true')
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-    // return false;
-  }
 }
 
 function EditCtrl($scope, $location, $routeParams, Page, Song) {
